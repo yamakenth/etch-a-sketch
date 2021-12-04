e7bfb3632c41fcc5739c146bf1bd1ffa3a96570c
Ken Yamada <yamakenth@gmail.com>
2021-12-04 11:34:11 +0700

    Add 16x16 grids

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-let helloWorld = document.querySelector('h1');
-console.log(helloWorld.innerText);
+let gridSize = 16;
+let gridContainer = document.querySelector('.grid-container');
+for (let i = 0; i < gridSize; i++) {
+  let gridRow = document.createElement('div');
+  gridRow.className = 'grid-row';
+  for (let j = 0; j < gridSize; j++) {
+    let grid = document.createElement('div');
+    grid.className = 'grid';
+    gridRow.appendChild(grid);
+  }
+  gridContainer.appendChild(gridRow);
+}
