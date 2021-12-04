44f1e08d622fd5a270c3d243d41ce31602729c12
Ken Yamada <yamakenth@gmail.com>
2021-12-04 21:07:35 +0700

    Add fixed size to grid

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 // create grids 
-let gridSize = 16;
-let gridContainer = document.querySelector('.grid-container');
-for (let i = 0; i < gridSize; i++) {
-  let gridRow = document.createElement('div');
-  gridRow.className = 'grid-row';
-  for (let j = 0; j < gridSize; j++) {
-    let grid = document.createElement('div');
-    grid.className = 'grid';
-    gridRow.appendChild(grid);
-  }
-  gridContainer.appendChild(gridRow);
-}
+const initialGridSize = 16;
+createGrids(initialGridSize);
 
 // change color of grid when hover 
 let grids = document.querySelectorAll('.grid');
@@ -18,7 +8,65 @@ grids.forEach((grid) => {
   grid.addEventListener('mouseenter', changeColor);
 }); 
 
-// function to change color of each grid 
+/*
+// reset and resize grid 
+let clearButton = document.querySelector('.clear-button');
+clearButton.addEventListener('click', () => {
+  // *************** delete old grids
+  deleteGrids();
+  // *************** delete old grids
+  
+  // reset
+  let grids = document.querySelectorAll('.grid');
+  grids.forEach((grid) => grid.style.backgroundColor = '#ffffff');
+  // resize 
+  let newGridSize = 0;
+  do {
+    let input = prompt('Enter desired grid size. Grid size must be between 1 and 100');
+    if (input === null) {
+      break;
+    }
+    newGridSize = parseInt(input);
+  } while (isNaN(newGridSize) || newGridSize <= 0 || newGridSize > 100);
+  createGrids(newGridSize);
+  
+});
+*/
+
+// create grids 
+// take in grid size 
+// return no results 
+function createGrids(gridSize) {
+  let gridContainer = document.querySelector('.grid-container');
+  for (let i = 0; i < gridSize; i++) {
+    let gridRow = document.createElement('div');
+    gridRow.className = 'grid-row';
+    for (let j = 0; j < gridSize; j++) {
+      let grid = document.createElement('div');
+      grid.className = 'grid';
+      gridRow.appendChild(grid);
+    }
+    gridContainer.appendChild(gridRow);
+  }
+}
+
+// change color of each grid area 
+// take in no parameters
+// return no results 
 function changeColor() {
   this.style.backgroundColor = '#000000';
 }
+
+// delete grids 
+// take in no parameters
+// return no results 
+/*
+function deleteGrids() {
+  let gridContainer = document.querySelector('.grid-container');
+  let gridContainerChild = gridContainer.lastElementChild;
+  while (gridContainerChild) {
+    gridContainer.removeChild(gridContainerChild);
+    gridContainerChild = gridContainer.lastElementChild;
+  }
+}
+*/
